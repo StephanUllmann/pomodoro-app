2059f29565c5f85cffd1910c1f9c04a1ae8bb692
Stephan Ullmann <stephan.ullmann@posteo.de>
2023-02-24 21:45:26 +0100

    Styling components complete

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 const timerInnerEl = document.querySelector(".timer__inner");
 const settingsIconEl = document.querySelector(".settings__icon");
 const overlayEl = document.querySelector(".overlay");
-const settingsEl = document.querySelector(".settings__menu");
+const settingsHideEl = document.getElementById("settings-hide");
+const applyBtnEl = document.querySelector(".btn--settings");
+
+const timerArcEl = document.getElementById("timer-arc");
 
 //////////////
 // Code snippet from [@opsb](https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle)
@@ -40,29 +43,49 @@ function describeArc(x, y, radius, startAngle, endAngle) {
   return d;
 }
 
-window.onload = function () {
-  const centerX = timerInnerEl.offsetWidth / 2;
-  const centerY = timerInnerEl.offsetHeight / 2;
-  const radius = centerX * 0.9;
-  const startAngle = 0;
-  const endAngle = 359;
+// Snippet End
 
-  document
-    .getElementById("timer-arc")
-    .setAttribute(
+const openModal = function () {
+  overlayEl.classList.remove("hidden");
+  settingsHideEl.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  overlayEl.classList.add("hidden");
+  settingsHideEl.classList.add("hidden");
+};
+
+const setupListeners = function () {
+  settingsIconEl.addEventListener("click", () => {
+    openModal();
+  });
+
+  overlayEl.addEventListener("click", () => {
+    closeModal();
+  });
+
+  applyBtnEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeModal();
+  });
+
+  window.addEventListener("DOMContentLoaded", function () {
+    const centerX = timerInnerEl.offsetWidth / 2;
+    const centerY = timerInnerEl.offsetHeight / 2;
+    const radius = centerX * 0.9;
+    const startAngle = 0;
+    const endAngle = 359.9;
+
+    timerArcEl.setAttribute(
       "d",
       describeArc(centerX, centerY, radius, startAngle, endAngle)
     );
+  });
 };
 
-// Snippet End
+const init = function () {
+  setupListeners();
+  openModal();
+};
 
-settingsIconEl.addEventListener("click", () => {
-  overlayEl.classList.remove("hidden");
-  settingsEl.classList.remove("hidden");
-});
-
-overlayEl.addEventListener("click", () => {
-  overlayEl.classList.add("hidden");
-  settingsEl.classList.add("hidden");
-});
+init();
